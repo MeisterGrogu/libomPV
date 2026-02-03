90a1459880bd41dea398debcc51eeea629cd64c1
Dominik Engelen <dominik.engelen@online.de>
2026-02-03 19:00:29 +0100

    feat(added README + styles)

---
+++ lib/styles/colors.dart
@@ -0,0 +1,5 @@
+import "package:flutter/material.dart";
+
+class sColors {
+  static const greyWhite = Color(0xffFAFBFC);
+}
---
+++ lib/styles/sizes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPadd2 = EdgeInsets.all(2);
+const kPadd4 = EdgeInsets.all(4);
+const kPadd8 = EdgeInsets.all(8);
+const kPadd10 = EdgeInsets.all(10);
